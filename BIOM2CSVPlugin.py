3b65a5df295126df7b22a9f3d43d62e423321170
verociraptor <veronica.parra1@fiu.edu>
2019-02-04 22:05:24 -0500

    Updated python file, input and output file

--- BIOM2CSVPlugin.py
+++ BIOM2CSVPlugin.py
@@ -10,8 +10,9 @@ class BIOM2CSVPlugin:
 
 	def run(self):
 		table = biom.load_table(self.myfile)
+		print table[0,0]
 		self.df = table.to_dataframe()
-
+		print dir(self.df)
 
 	def output(self, filename):
-		self.df.to_csv(filename)
+		self.df.to_csv(filename, index=False)
